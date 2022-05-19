a82d5ab7758f5dec1f45ef490699ea7a1f683f3a
Fagner Schwalm <fagnerschwalm@gmail.com>
2022-05-19 18:39:51 -0300

    test: login component

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -3,25 +3,34 @@
 
 import * as React from 'react'
 import {render, screen} from '@testing-library/react'
+import faker from 'faker';
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
 
-test('submitting the form calls onSubmit with username and password', () => {
-  // 🐨 create a variable called "submittedData" and a handleSubmit function that
-  // accepts the data and assigns submittedData to the data that was submitted
-  // 💰 if you need a hand, here's what the handleSubmit function should do:
-  // const handleSubmit = data => (submittedData = data)
-  //
-  // 🐨 render the login with your handleSubmit function as the onSubmit prop
-  //
-  // 🐨 get the username and password fields via `getByLabelText`
-  // 🐨 use `await userEvent.type...` to change the username and password fields to
-  //    whatever you want
-  //
-  // 🐨 click on the button with the text "Submit"
-  //
-  // assert that submittedData is correct
-  // 💰 use `toEqual` from Jest: 📜 https://jestjs.io/docs/en/expect#toequalvalue
+const buildLoginForm = () => ({
+  username: faker.internet.userName(),
+  password: faker.internet.password(),
+})
+
+test('submitting the form calls onSubmit with username and password', async () => {
+  const handleSubmit = jest.fn()
+
+  render(<Login onSubmit={handleSubmit} />)
+
+  const usernameField = screen.getByLabelText(/username/i)
+  const passwordField = screen.getByLabelText(/password/i)
+  const submitButton = screen.getByRole('button', {
+    name: /submit/i,
+  })
+
+  const {username, password} = buildLoginForm()
+
+  await userEvent.type(usernameField, username)
+  await userEvent.type(passwordField, password)
+  await userEvent.click(submitButton)
+
+  expect(handleSubmit).toHaveBeenCalledWith({password, username})
+  expect(handleSubmit).toBeCalledTimes(1)
 })
 
 /*
